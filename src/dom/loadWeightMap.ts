b983a66fa840f1fcb12b5377171c2f5e30c644ed
Simon Dean <simon@simondean.org>
2023-03-08 16:04:30 +0000

    tfjs-node

--- src/dom/loadWeightMap.ts
+++ src/dom/loadWeightMap.ts
@@ -1,4 +1,4 @@
-import * as tf from '@tensorflow/tfjs-node';
+import * as tf from '@tensorflow/tfjs-core';
 
 import { getModelUris } from '../common/getModelUris';
 import { fetchJson } from './fetchJson';
